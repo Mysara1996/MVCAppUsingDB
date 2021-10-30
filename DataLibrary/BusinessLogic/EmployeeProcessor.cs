ccd394f066430d40b04de531df64225e06801ab7
Maysara1996 <m.alanani12@gmail.com>
2021-10-30 23:46:42 +0200

    Add project files.

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,37 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId, String firstName, String lastName, String EmailAddress)
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = EmailAddress
+            };
+
+            string sql = @"insert into dbo.Employee (EmployeeId, FirstName, LastName, EmailAddress)
+                          values (@EmployeeId, @FirstName, @LastName, @EmailAddress);";
+           
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployess() 
+        {
+            String sql = @"select Id, EmployeeId, FirstName, LastName, EmailAddress
+                         from dbo.Employees;";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
---
+++ MVCDemoDB/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(6, 2) , 
+    [EmployeeId] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastNAme] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(50) NOT NULL
+)
